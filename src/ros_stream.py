e96731dd3f54efe49e188cd930773184689f6c1f
Andrea Santangelo <asantangelo90@gmail.com>
2021-10-05 15:12:02 +0200

    Added a try-catch in the audio receiver.

--- src/ros_stream.py
+++ src/ros_stream.py
@@ -546,67 +546,68 @@ if __name__ == '__main__':
     set_volume_timer()
 
     while True:
-        ##print("####### Node is listening #######")
-        data, address = s.recvfrom(8192)
-        #if len(data) < 5:
-        #    try:
-        #        new_volume_multiplier = int(data)
-        #        if 1 < new_volume_multiplier < 50:
-        #            volume_multiplier = new_volume_multiplier
-        #        elif new_volume_multiplier <= 1:
-        #            volume_multiplier = 1
-        #        else:
-        #            volume_multiplier = 50
-        #        print("New volume multiplier is: {}".format(volume_multiplier))
-        #    except:
-        #        print("Received a wrong value for volume_multiplier.")
-        #    continue
-        packet = Packets.getPacketFromBytes(data)
+        try:
+            ##print("####### Node is listening #######")
+            data, address = s.recvfrom(8192)
+            #if len(data) < 5:
+            #    try:
+            #        new_volume_multiplier = int(data)
+            #        if 1 < new_volume_multiplier < 50:
+            #            volume_multiplier = new_volume_multiplier
+            #        elif new_volume_multiplier <= 1:
+            #            volume_multiplier = 1
+            #        else:
+            #            volume_multiplier = 50
+            #        print("New volume multiplier is: {}".format(volume_multiplier))
+            #    except:
+            #        print("Received a wrong value for volume_multiplier.")
+            #    continue
+            packet = Packets.getPacketFromBytes(data)
 
-        if (packet.Destination == GetIp(config['GENERAL']['InterfaceRasp'])):
-            if(int(packet.Type) == 2):
-                elements = bytearray(packet.Payload)
-                bytes_to_write = bytearray()
-                temp_bytes = bytearray()
-                bytes_effectively_written = 0
+            if (packet.Destination == GetIp(config['GENERAL']['InterfaceRasp'])):
+                if(int(packet.Type) == 2):
+                    elements = bytearray(packet.Payload)
+                    bytes_to_write = bytearray()
+                    temp_bytes = bytearray()
+                    bytes_effectively_written = 0
 
-                i = 0
-                # The format of the bytes in the packet is as follow:
-                # 0 s1msb s1lsb 0 s2msb s2lsb 0 .... 0 sNmsb sNlsb
-                while i < len(elements):
-                    if elements[i] != 48: # our separator is b'/x30' which is 48 in ASCII (literal '0')
-                        temp_bytes.extend(elements[i:i+1])
-                        
-                    elif elements[i] == 48:
-                        if len(temp_bytes) == 2:
-                            integer = int.from_bytes(temp_bytes, "big", signed="True")
-                            integer *= volume_multiplier
-                            try:
-                                new_bytes = integer.to_bytes(2, 'big', signed=True)
-                            except:
-                                if integer > 0:
-                                    integer = 30000
-                                else:
-                                    integer = -30000
-                                new_bytes = integer.to_bytes(2, 'big', signed=True)
-                            bytes_to_write.extend(new_bytes)
-                            bytes_effectively_written += 2
-                        temp_bytes = bytearray()
-                    i += 1
-                
-                
-                # This is to recover the last sample "sNmsb sNlsb"
-                if len(temp_bytes) == 2:
-                        bytes_to_write.extend(temp_bytes[0:2])
-                        bytes_effectively_written += 2        
-                
-                if len(bytes_to_write) > 0:
-                    # We don't write if we are not receiving (or 5 times neutral writing have been executed)  
-                    #print("Byte-Writer: byte sent ", len(bytes_to_write))
-                    f = open("/dev/shm/{}.bin".format(packet.Source), "ab")
-                    f.write(bytes_to_write)
-                    f.close()
-                
-                #print("audio-receiver: Data Received from: ", packet.Source, ' Size: ', len(elements), ' Effect: ', bytes_effectively_written)
-    
-
+                    i = 0
+                    # The format of the bytes in the packet is as follow:
+                    # 0 s1msb s1lsb 0 s2msb s2lsb 0 .... 0 sNmsb sNlsb
+                    while i < len(elements):
+                        if elements[i] != 48: # our separator is b'/x30' which is 48 in ASCII (literal '0')
+                            temp_bytes.extend(elements[i:i+1])
+                            
+                        elif elements[i] == 48:
+                            if len(temp_bytes) == 2:
+                                integer = int.from_bytes(temp_bytes, "big", signed="True")
+                                integer *= volume_multiplier
+                                try:
+                                    new_bytes = integer.to_bytes(2, 'big', signed=True)
+                                except:
+                                    if integer > 0:
+                                        integer = 30000
+                                    else:
+                                        integer = -30000
+                                    new_bytes = integer.to_bytes(2, 'big', signed=True)
+                                bytes_to_write.extend(new_bytes)
+                                bytes_effectively_written += 2
+                            temp_bytes = bytearray()
+                        i += 1
+                    
+                    
+                    # This is to recover the last sample "sNmsb sNlsb"
+                    if len(temp_bytes) == 2:
+                            bytes_to_write.extend(temp_bytes[0:2])
+                            bytes_effectively_written += 2        
+                    
+                    if len(bytes_to_write) > 0:
+                        # We don't write if we are not receiving (or 5 times neutral writing have been executed)  
+                        #print("Byte-Writer: byte sent ", len(bytes_to_write))
+                        f = open("/dev/shm/{}.bin".format(packet.Source), "ab")
+                        f.write(bytes_to_write)
+                        f.close()
+                    
+                    #print("audio-receiver: Data Received from: ", packet.Source, ' Size: ', len(elements), ' Effect: ', bytes_effectively_written)
+        except Exception as e:
+            print(e)
